07cc42565222145e99b98d1a7ca081f3cf0801ab
c4bae <123281198+c4bae@users.noreply.github.com>
2025-07-18 13:18:18 -0700

    reorganized workspace, created various directories

--- src/home/script.js
+++ src/home/script.js
@@ -1,3 +1,5 @@
+import "style.css";
+
 function togglePop() {
     const popup = document.querySelector(".info-popup");
     popup.classList.toggle('active');
--- webpack.dev.js
+++ webpack.dev.js
@@ -6,6 +6,6 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: "eval-source-map",
     devServer: {
-    watchFiles: ["./src/template.html"],
+    watchFiles: ["./src/index.html"],
   },
 });
